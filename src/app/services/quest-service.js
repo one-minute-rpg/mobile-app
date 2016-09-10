888fa91fbdf9e12ab6fdb6fff5a67d7686063e8c
Ben-hur Santos Ott <benhur.ott@gmail.com>
2016-09-10 16:19:28 -0300

    feat(quest)
    Adding events.

--- src/app/services/quest-service.js
+++ src/app/services/quest-service.js
@@ -1,15 +1,35 @@
 /**
  * Created by ben-hur on 15/08/2016.
  */
-function OmrQuestService($http, $q, $timeout, translationService) {
+function OmrQuestService($http, $q, $timeout, translationService, localStorageService) {
+    function _getSaveGameKey(questId) {
+        return 'SAVE_GAME_QUEST_' + questId;
+    }
+
+
     return {
         getQuest: function (questId) {
             return $http.get('resources/$quests/' + questId + '/' + questId + '.json')
                 .then(function(questData){
                     return questData.data;
                 });
+        },
+
+        saveGame: function(questId, character, currentSceneId) {
+            var key = _getSaveGameKey(questId);
+
+            localStorageService.setObject(key, {
+                questId: questId,
+                character: character,
+                currentSceneId: currentSceneId
+            });
+        },
+
+        getSavedGame: function(questId) {
+            var key = _getSaveGameKey(questId);
+            return localStorageService.getObject(key);
         }
     }
 }
 
-angular.module('omr').factory('questService', ['$http', '$q', '$timeout', 'translationService', OmrQuestService]);
+angular.module('omr').factory('questService', ['$http', '$q', '$timeout', 'translationService', 'localStorageService', OmrQuestService]);
--- src/www/app.bundle.js
+++ src/www/app.bundle.js
@@ -102,18 +102,38 @@ angular.module('omr').factory('platformService', ['ENVIRONMENT', PlatformService
 /**
  * Created by ben-hur on 15/08/2016.
  */
-function OmrQuestService($http, $q, $timeout, translationService) {
+function OmrQuestService($http, $q, $timeout, translationService, localStorageService) {
+    function _getSaveGameKey(questId) {
+        return 'SAVE_GAME_QUEST_' + questId;
+    }
+
+
     return {
         getQuest: function (questId) {
             return $http.get('resources/$quests/' + questId + '/' + questId + '.json')
                 .then(function(questData){
                     return questData.data;
                 });
+        },
+
+        saveGame: function(questId, character, currentSceneId) {
+            var key = _getSaveGameKey(questId);
+
+            localStorageService.setObject(key, {
+                questId: questId,
+                character: character,
+                currentSceneId: currentSceneId
+            });
+        },
+
+        getSavedGame: function(questId) {
+            var key = _getSaveGameKey(questId);
+            return localStorageService.getObject(key);
         }
     }
 }
 
-angular.module('omr').factory('questService', ['$http', '$q', '$timeout', 'translationService', OmrQuestService]);
+angular.module('omr').factory('questService', ['$http', '$q', '$timeout', 'translationService', 'localStorageService', OmrQuestService]);
 
 /**
  * Created by ben-hur on 15/08/2016.
@@ -350,12 +370,14 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
     var self = this;
 
     self.character = {
-    }
+    };
 
     self.currentScene = {
         title: '',
         text: ''
-    }
+    };
+
+    self.notifications = [];
 
     self.goHome = _goHome;
     self.TRANSLATIONS = {};
@@ -373,18 +395,35 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
 
     function _loadQuest() {
         questService.getQuest($stateParams.questId)
-            .then(function(questData) {
+            .then(function (questData) {
                 _quest = questData;
-                _loadCharacter();
-                _loadScene();
+                _loadSavedGame();
                 loaderService.hide();
             });
+    }
+
+    function _loadSavedGame() {
+        var savedGame = questService.getSavedGame(_quest.quest_id);
+
+        if (!!savedGame) {
+            self.character = angular.copy(savedGame.character);
+            $location.search({ sceneId: savedGame.currentSceneId });
+
+            _loadScene();
+        }
+        else {
+            _createNewGame();
+        }
+    }
+
+    function _createNewGame() {
+        self.character = angular.copy(_quest.character);
+        _loadScene();
     }
 
     /************************
      * SCENE
      ************************/
-
     function _loadScene() {
         var currentSceneId = $location.search().sceneId || 1;
         var sceneData = _getScene(Number(currentSceneId));
@@ -392,13 +431,13 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
     }
 
     function _bindScene(scene) {
-        var currentTranslationKey = self.TRANSLATIONS.$NAME;
         self.currentScene = _formatScene(scene);
     }
 
     function _formatScene(scene) {
         var currentTranslationKey = self.TRANSLATIONS.$NAME;
         var formattedScene = {
+            scene_id: scene.scene_id,
             title: scene.title[currentTranslationKey],
             text: scene.text[currentTranslationKey],
             actions: _getActions(scene)
@@ -412,7 +451,7 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
         return scene.actions
             .filter(_filterRequireAttributeAction)
             .filter(_filterRequireItemAction)
-            .map(function(act) {
+            .map(function (act) {
                 var formattedAction = angular.copy(act);
                 formattedAction.text = act.text[currentTranslationKey];
                 formattedAction._icon = _getActionIcon(act);
@@ -421,9 +460,9 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
     }
 
     function _filterRequireAttributeAction(action) {
-        if(action.require_attribute_value) {
+        if (action.require_attribute_value) {
             for (var attr in action.require_attribute_value) {
-                if(self.character.attributes[attr].current < action.require_attribute_value[attr]) {
+                if (self.character.attributes[attr].current < action.require_attribute_value[attr]) {
                     return false;
                 }
             }
@@ -435,13 +474,13 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
     function _filterRequireItemAction(action) {
         var showAction = true;
 
-        if(action.require_item_ids.length) {
-            action.require_item_ids.forEach(function(itemId) {
-                var notFound = self.character.itens.filter(function(i){
+        if (action.require_item_ids.length) {
+            action.require_item_ids.forEach(function (itemId) {
+                var notFound = self.character.itens.filter(function (i) {
                     return i.item_id == itemId && i.quantity > 0;
                 }).length == 0;
 
-                if(notFound) {
+                if (notFound) {
                     showAction = false;
                 }
             });
@@ -451,15 +490,18 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
     }
 
     function _getActionIcon(action) {
-        if(action.require_item_ids.length) {
+        if (action.require_item_ids.length) {
             return 'ITEM';
+        }
+        else if (action.require_attribute_value !== null) {
+            return 'STATUS';
         }
 
         return 'MAP';
     }
 
     function _getScene(sceneId) {
-        var scene = _quest.scenes.filter(function(s) {
+        var scene = _quest.scenes.filter(function (s) {
             return s.scene_id === sceneId;
         })[0];
 
@@ -469,44 +511,151 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
     /************************
      * CHARACTER
      ************************/
+    function _getCharacterItem(itemId) {
+        return self.character.itens.filter(function (item) {
+            return item.item_id == itemId;
+        })[0];
+    }
 
-    function _loadCharacter() {
-        self.character = angular.copy(_quest.character);
+    function _removeCharacterItem(itemId) {
+        self.character.itens = self.character.itens.filter(function (item) {
+            return item.item_id != itemId;
+        });
     }
 
 
+    /************************
+     * NOTIFICATIONS
+     ************************/
+    function _showNotificationsForEvents(events) {
+        if(!events || !events.length) return;
+
+        var currentTranslationKey = self.TRANSLATIONS.$NAME;
+
+        var notifications = events
+            .filter(function(e) {
+                return !!e.text[currentTranslationKey];
+            })
+            .map(function(e){
+                return {
+                    type: _getNotificationType(e),
+                    text: e.text[currentTranslationKey]
+                };
+            });
+
+        self.notifications= notifications;
+
+        $timeout(function(){
+            self.notifications = [];
+        }, 2000);
+    }
+
+    function _getNotificationType(event) {
+        switch(event.event_type) {
+            case 'ADD_ITEM':
+            case 'REMOVE_ITEM':
+                return 'ITEM';
+            case 'CHANGE_ATTRIBUTE':
+                return 'STATUS';
+            default:
+                return 'ALERT';
+        };
+    }
 
 
     /************************
      * ACTIONS
      ************************/
-
     function _onChoseAction(action) {
         var events = action.events;
+        _showNotificationsForEvents(events);
 
-        events.forEach(function(e) {
-            if(e.event_type === 'CHANGE_ATTRIBUTE') {
-                _changeAttribute(e.attribute, e.value);
+        events.forEach(function (e) {
+            if (e.event_type === 'CHANGE_ATTRIBUTE') {
+                _eventChangeAttribute(e.attribute, e.value);
             }
-            else if(e.event_type === 'GO_TO_SCENE') {
-                _goToScene(e.scene_id);
+            else if (e.event_type === 'ADD_ITEM') {
+                _eventAddItem(e.item_id, e.quantity);
+            }
+            else if (e.event_type === 'REMOVE_ITEM') {
+                _eventRemoveItem(e.item_id, e.quantity);
+            }
+            else if (e.event_type === 'GO_TO_SCENE') {
+                _eventGoToScene(e.scene_id);
+            }
+            else if (e.event_type === 'SAVE_GAME') {
+                _eventSaveGame();
             }
         });
     }
 
-    function _goToScene(sceneId) {
-        $location.search({sceneId: sceneId});
+    function _eventSaveGame() {
+        questService.saveGame(
+            _quest.quest_id,
+            self.character,
+            self.currentScene.scene_id
+        );
+    }
+
+    function _eventGoToScene(sceneId) {
+        $location.search({ sceneId: sceneId });
         _loadScene();
     }
 
-    function _changeAttribute(attribute, value) {
-        self.character.attributes[attribute].current += value;
+    function _eventChangeAttribute(attribute, value) {
+        var refAttribute = self.character.attributes[attribute];
+        refAttribute.current += value;
 
-        if(self.character.attributes[attribute].current > self.character.attributes[attribute].max) {
-            self.character.attributes[attribute].current = self.character.attributes[attribute].max;
+        if (refAttribute.current > refAttribute.max) {
+            refAttribute.current = refAttribute.max;
         }
     }
 
+    function _eventAddItem(itemId, quantity) {
+        var questItem = _quest.itens.filter(function (item) {
+            return item.item_id == itemId;
+        })[0];
+
+        var charItem = _getCharacterItem(itemId);
+
+        if (charItem) {
+            self.character.itens[
+                self.character.itens.indexOf(charItem)
+            ].quantity += quantity;
+        }
+        else {
+            self.character.itens.push({
+                item_id: questItem.item_id,
+                quantity: quantity
+            });
+        }
+    }
+
+    function _eventRemoveItem(itemId, quantity) {
+        if (quantity == 0) {
+            _removeCharacterItem(itemId);
+        }
+        else {
+            var charItem = _getCharacterItem(itemId);
+            if (charItem) {
+                charItem.quantity -= quantity;
+
+                if (charItem.quantity <= 0) {
+                    _removeCharacterItem(itemId);
+                }
+                else {
+                    self.character.itens[
+                        self.character.itens.indexOf(charItem)
+                    ] = charItem;
+                }
+            }
+        }
+    }
+
+
+    /************************
+     * ROUTE
+     ************************/
     function _goHome() {
 
     }
@@ -515,10 +664,10 @@ function QuestPageController($stateParams, $timeout, $location, platformService,
 angular.module('omr').component('quest', {
     templateUrl: 'components/pages/quest/quest.html',
     controller: [
-        '$stateParams', 
+        '$stateParams',
         '$timeout',
         '$location',
-        'platformService', 
+        'platformService',
         'loaderService',
         'soundService',
         'SOUNDS',
@@ -543,6 +692,27 @@ angular.module('omr').component('omrActionBar', {
         home: '=',
         audio: '=',
         store: '='
+    }
+});
+
+/**
+ * Created by ben-hur on 21/08/2016.
+ */
+function OmrBackButtonController($) {
+    var self = this;
+
+    self.__onClick = function () {
+        if(!!self.onClick) {
+            self.onClick();
+        }
+    }
+}
+
+angular.module('omr').component('omrBackButton', {
+    templateUrl: 'components/shared/back-button/back-button.html',
+    controller: [OmrBackButtonController],
+    bindings: {
+        onClick: '&'
     }
 });
 
@@ -645,27 +815,6 @@ angular.module('omr').component('omrConfigurations', {
         'EVENTS',
         OmrConfigurationsController],
     bindings: {
-    }
-});
-
-/**
- * Created by ben-hur on 21/08/2016.
- */
-function OmrBackButtonController($) {
-    var self = this;
-
-    self.__onClick = function () {
-        if(!!self.onClick) {
-            self.onClick();
-        }
-    }
-}
-
-angular.module('omr').component('omrBackButton', {
-    templateUrl: 'components/shared/back-button/back-button.html',
-    controller: [OmrBackButtonController],
-    bindings: {
-        onClick: '&'
     }
 });
 
